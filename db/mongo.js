134789ced8de989b8ec91811eddf9dc34d7bfa38
gaurav pal <gauravpal541@gmail.com>
2022-04-24 02:44:38 +0530

    s

--- db/mongo.js
+++ db/mongo.js
@@ -1,21 +1,13 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const url = 'mongodb+srv://fullstack:12345@cluster0.pbhgs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const url = process.env.MONGODB_URI
 
   mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: Number,
-        minLength: 8,
-        required: true
-    }
+    name: String,
+    number: Number
   }).set('toJSON', {
     transform: (document, returnedObject) => {
       returnedObject.id = returnedObject._id.toString()
