5d3454fc4ac9291d9ca27841eca050a86135fed3
gaurav pal <gauravpal541@gmail.com>
2022-04-23 21:08:56 +0530

    index.js improved

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 
 app.use(morgan((tokens, req, res) => {
   return [
